61df3ec038a0dc1c6346316b0322ea2fabb70e1f
Kayla Cross <kac288@georgetown.edu>
2018-03-25 23:51:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
   def self.new_by_filename(filename)
     file_array = filename.chomp('.mp3').split(' - ')
     song = self.new(file_array[1])
-    song.artist = file_array[0]
+    song.artist = Artist.find_or_create_by_name(file_array[0])
     song.genre = file_array[2]
     binding.pry
   end
