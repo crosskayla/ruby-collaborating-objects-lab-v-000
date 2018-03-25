8e74857e1de02d51d04d2b16edccad3d3123a285
Kayla Cross <kac288@georgetown.edu>
2018-03-25 23:46:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song = self.new(filename.chomp('mp3').split(' - ')[1]
+    song = self.new(filename.chomp('mp3').split(' - ')[1])
     song.artist = filename.chomp('mp3').split(' - ')[0]
     song.genre = filename.chomp('mp3').split(' - ')[2]
   end
