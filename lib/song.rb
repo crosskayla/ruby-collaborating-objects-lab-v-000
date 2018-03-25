d32596e45fcf00941d74b7a9c1a9608914c934fd
Kayla Cross <kac288@georgetown.edu>
2018-03-25 23:48:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
 
   attr_accessor :name, :artist, :genre
@@ -11,6 +13,7 @@ class Song
     song = self.new(file_array[1])
     song.artist = file_array[0]
     song.genre = file_array[2]
+    binding.pry
   end
 
 end
